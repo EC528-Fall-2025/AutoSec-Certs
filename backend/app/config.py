8714b099df028b41d8d4f7d0fa2b5fdf7c175f3a
ChingSsuyuan <crazycyy233@gmail.com>
2025-10-01 19:46:53 -0400

    backend update

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,22 @@
 # Configuration and Environment Variables
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # FastAPI
+    PORT: int = 8000
+    API_KEY: str = "secret-key-123"
+    
+    VAULT_ADDR: str = "http://localhost:8200"
+    VAULT_TOKEN: str = "dev-token"
+    VAULT_PKI_PATH: str = "pki"
+    
+    SERVICENOW_INSTANCE: str = "https://dev12345.service-now.com"
+    SERVICENOW_USERNAME: str = "admin"
+    SERVICENOW_PASSWORD: str = "password"
+    SERVICENOW_TABLE: str = "x_custom_cert_requests"
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
